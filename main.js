5bc95ae9747c52eb50b49b8cc3246d27c5a6df06
道小铿 <iRoader@163.com>
2015-08-04 14:40:39 +0800

    music
    使用angularJS做的一个音乐播放展示!

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+var app = angular.module("myApp", []);
+
+app.controller("PlayerController", ['$scope', function($scope){
+	$scope.playing = false;
+	$scope.audio = document.createElement('audio');
+	$scope.audio.src = 'sky.mp3';
+
+	$scope.play = function(){
+		$scope.audio.play();
+		$scope.playing = true;
+	};
+	$scope.stop = function(){
+		$scope.audio.pause();
+		$scope.playing = false;
+	}
+	$scope.audio.addEventListener('ended', function(){
+		$scope.$apply(function(){
+			$scope.stop();
+		});
+	});
+
+}]);
+
+
+app.controller('RelatedController', ['$scope', function($scope) {
+
+}]);
